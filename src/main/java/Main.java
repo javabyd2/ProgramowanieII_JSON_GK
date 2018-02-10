92406ad6035ab03288588b4439caf03b0e07580d
Grzegorz Kubiak <kingregor@gmail.com>
2018-02-10 10:54:04 +0100

    z zapisem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.Item;
+import model.Order;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        
+
+    //private static String pathname = "C:\\Users\\A666224\\Desktop\\JAVA\\";
+
+    public static void main(String[] args) throws IOException {
+        Item item1 = new Item("mleko","2","tesco");
+        Item item2 = new Item("masło","5","tesco");
+        Item item3 = new Item("chleb","2","tesco");
+
+        List<Item> itemList = Arrays.asList(item1,item2,item3);
+        Order order = new Order(itemList,1);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        File filename = new File("order.json");
+
+        filename.createNewFile();
+        //Object to JSON in file
+        mapper.writeValue(filename, order);
+
+        //Object to JSON in String
+        //String jsonInString = mapper.writeValueAsString(z.pozycje);
+
     }
 }
